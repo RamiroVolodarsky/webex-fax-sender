40cc3c9232224e74f16d2385de911559d40a9766
Ramiro Volodarsky <rvolodarsky@ipfone.com>
2023-01-26 14:51:27 -0300

    Cleaning code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Login.module.css'
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function Login() {
 
